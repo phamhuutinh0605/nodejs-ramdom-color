7cc56b29040c2781bda05cd39e3908a30080b32b
phamhuutinh0605 <90019287+phamhuutinh0605@users.noreply.github.com>
2024-06-04 17:03:26 +0700

    Update Command.sh

--- Command.sh
+++ Command.sh
@@ -1,9 +1,9 @@
 #-------------------------------------------
-#Lab7 - Docker compose with load balancer
-#Start docker compose
+#Lab7 - Docker compose with load balancer.
+#Start docker compose.
 docker-compose -f docker-compose-load-balance.yaml up -d
 #[Optional] Force rebuild dockerfile.
 docker-compose -f docker-compose-load-balance.yaml up -d --build
-#Terminate all resource
+#Terminate all resource.
 docker-compose -f docker-compose-load-balance.yaml down
 
